8994ebeeeb41fe531ff065cb0c8b2b7e35ab4b88
Patrick Oscity <patrick.oscity@gmail.com>
2011-12-22 11:57:09 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,67 @@
+var open = null;
+
+var easydate_options = {
+  locale: { 
+    "future_format": "%s %t", 
+    "past_format": "%s %t", 
+    "second": "Sekunde", 
+    "seconds": "Sekunden", 
+    "minute": "Minute", 
+    "minutes": "Minuten", 
+    "hour": "Stunde", 
+    "hours": "Stunden", 
+    "day": "Tag", 
+    "days": "Tage", 
+    "week": "Woche", 
+    "weeks": "Wochen", 
+    "month": "Monat", 
+    "months": "Monate", 
+    "year": "Jahr", 
+    "years": "Jahre", 
+    "yesterday": "Gestern", 
+    "tomorrow": "Morgen", 
+    "now": "Gerade eben", 
+    "ago": "Vor", 
+    "in": "In" 
+  }
+};
+
+update = function(){
+  $('body').animate({backgroundColor:'#f5f5f5'}, 500);
+
+  jQuery.ajax({
+    type: 'GET',
+    dataType: 'json',
+    timeout: 1000,
+    url: 'http://twitter.com/statuses/user_timeline/mr_door_status.json?callback=?&count=1',
+    success: function(tweets) {
+      var text = tweets[0].text;
+      var date = new Date( tweets[0].created_at );
+      var now  = new Date;
+      var num  = text.split("No. ")[1];
+  
+      if ( text.match(/.*offen.*/) ) {
+        $('#status').html('offen');
+        $('body').animate({backgroundColor:'#E7F5BC'}, 500);
+      } else {
+        $('#status').html('geschlossen');
+        $('body').animate({backgroundColor:'#F5A9A9'}, 500);
+      }
+      $('#error').html('&nbsp;');
+      $('#checked .easydate').attr('title', now.toUTCString());
+      $('.easydate').easydate(easydate_options);
+    },
+    error: function(tweets) {
+      $('#error').html('(keine Verbindung)');
+    }
+  });
+
+  return false;
+}
+
+$(document).ready(function(){
+  setTimeout(update, 200);
+  setInterval(update, 5*60*1000);
+
+  $('#update-button').click(update);
+});
